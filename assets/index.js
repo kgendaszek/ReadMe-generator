c0354a694ea961e4b1702710d915c6ace65deec6
KatieG <katieshake27@gmail.com>
2021-11-29 22:32:21 -0500

    started questions

--- assets/index.js
+++ assets/index.js
@@ -5,15 +5,28 @@ const fs = require('fs');
 const questions = () =>{
     return inquirer.prompt([
         {
-            
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 
         }
-    ];
+    ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(project, data) {
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+    .then((answers)=>fs.writeFileSync('README.md', generateRead(answers)))
+    .then(()=> console.log("Generated README"))
+    .catch((err)=> console.error(err));
+};
 
 // Function call to initialize app
 init();
